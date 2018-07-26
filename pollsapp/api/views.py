b5a4c0b5a54e91f4f70b45197065bac846aaf11e
karthikraja34 <rajakarthik131@gmail.com>
2018-07-26 16:17:07 +0530

    Add results endpoint

--- pollsapp/api/views.py
+++ pollsapp/api/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 from ..models import Question,Choice
 from .serializers import QuestionSerializer,QuestionListSerializer
 
@@ -28,7 +29,7 @@ class QuestionDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-# @method_decorator(csrf_exempt, name='dispatch')
+
 class VoteView(APIView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -36,9 +37,33 @@ class VoteView(APIView):
 
     def post(self, request):
         choice = request.data['choice']
-        # question = Question.objects.get(pk = request.data['question_id'])
-        question = get_object_or_404(Question, pk=request.data['question_id'])
-        selected_choice = question.choice.get(pk=choice)
+        try:
+            question = get_object_or_404(Question, pk=request.data['question_id'])
+        except ValidationError:
+            return JsonResponse({'data': 'not found'})
+        try:
+            selected_choice = question.choice.get(pk=choice)
+        except (KeyError, Choice.DoesNotExist):
+            return JsonResponse({'data': 'choice does not exist'})
         selected_choice.votes += 1
         selected_choice.save()
         return JsonResponse({'choice': request.data['choice'],'votes': selected_choice.votes})
+
+class ResultView(APIView):
+    def get(self,request,pk):
+        try:
+            question = get_object_or_404(Question, pk=pk)
+        except ValidationError:
+            return JsonResponse({'data': 'not found'})
+        try:
+            selected_choice = question.choice.filter(question=question)
+        except (KeyError, Choice.DoesNotExist):
+            return JsonResponse({'data': 'choice does not exist'})
+        dic = {}
+        li = [{choice.choice_text:choice.votes} for choice in selected_choice]
+
+        # for choice in selected_choice:
+            # dic['choice'] = choice.choice_text
+            # dic['vote'] = choice.votes
+            # li.append(dic)
+        return JsonResponse({'data': li})
--- pollsapp/urls.py
+++ pollsapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/',view=views.QuestionListCreateView.as_view(),name = 'question_list_api'),
     path('api/<pk>',view=views.QuestionDetailView.as_view(),name = 'question_detail_api'),
     path('api/vote/',view=views.VoteView.as_view(),name="vote_view"),
+    path('api/<pk>/results',view=views.ResultView.as_view(),name="vote_view"),
 ]
